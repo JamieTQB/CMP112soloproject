c2b043b9307c2e21e6805bf24a0ec7329d421b32
Jamie M 1904059 <maccallumjamie@gmail.com>
2025-12-11 04:58:20 +0000

    changed input system
    changed the input system to the new input system, added a physics material for the floor

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class playerController : MonoBehaviour
@@ -9,35 +10,44 @@ public class playerController : MonoBehaviour
     public float speed;
     public float boostSpeed;
     bool boosting;
+    float moveForwards;
+    float moveSideways;
     Vector3 movement;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>(); 
     }
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        moveForwards = movementVector.y;
+        moveSideways = movementVector.x;
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 movement = Vector3.zero;
-        if (Input.GetKey(KeyCode.W))
+        movement = new Vector3(moveSideways, 0.0f, moveForwards);
+
+        rb.AddForce(movement * speed);
+        //Vector3 movement = Vector3.zero;
+        if (moveForwards == 0)
         {
-            movement = rb.transform.forward;
+            rb.linearVelocity = Vector3.zero;
         }
-        if (Input.GetKey(KeyCode.S))
-        {
-            movement = -rb.transform.forward;
-        }
-        if (Input.GetKey(KeyCode.A))
+        if (moveSideways < 0)
         {
             rb.rotation = Quaternion.Euler(0, 0, 45).normalized;
-            movement = -rb.transform.right;
+            //movement = -rb.transform.right;
         }
-        if (Input.GetKey(KeyCode.D))
+        
+        if (moveSideways > 0)
         {
             rb.rotation = Quaternion.Euler(0, 0, -45).normalized;
-            movement = rb.transform.right;
+            //movement = rb.transform.right;
         }
+        /*
         if (Input.GetKeyDown(KeyCode.Space))
         {
             StartCoroutine(Boost());
@@ -46,11 +56,12 @@ public class playerController : MonoBehaviour
         if (Input.GetKey(KeyCode.Escape))
         {
             SceneManager.LoadScene("Menu");
-        }
-        if (!(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
+        }*/
+        if (moveSideways == 0)
         {
             rb.rotation = Quaternion.identity;
-        }
+            rb.linearVelocity = Vector3.zero;
+        }/*
         if (boosting)
         {
             movement = movement.normalized * speed * boostSpeed;
@@ -60,11 +71,11 @@ public class playerController : MonoBehaviour
         {
             movement = movement.normalized * speed;
             rb.linearVelocity = new Vector3(movement.x, rb.linearVelocity.y, movement.z);
-        }
-       
-        
-    }
+        }*/
 
+
+    }
+    
     IEnumerator Boost()
     {
         yield return new WaitForSeconds(1f);
